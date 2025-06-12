32c493a3370e57e227509e98afa54370c8ac527b
MunSeongJun5133 <ans1332@naver.com>
2025-06-12 19:47:05 +0900

    초기 커밋.

---
+++ 13241_LeastCommonMultiple2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _13241_LeastCommonMultiple2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] str = Console.ReadLine().Split();
+            Int64 a = Int32.Parse(str[0]);
+            Int64 b = Int32.Parse(str[1]);
+
+            Int64 oa = a;
+            Int64 ob = b;
+            Int64 lm;
+
+            while (true)
+            {
+                if(a % b == 0)
+                {
+                    lm = b;
+                    break;
+                }
+
+                Int64 tmp = b;
+                b = a % b;
+                a = tmp;
+            }
+
+            lm = (oa * ob) / lm;
+
+            Console.WriteLine(lm);
+        }
+    }
+}
